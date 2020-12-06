e52af2a943a3c3dad3b45e2954a2da0b0cedbafc
ollinich <oliver.nicholson@gmail.com>
2020-12-06 12:52:55 +0000

    Adding Index assertions in tests, refactoring code to pass all previous test scenarios, added failing fifth scenario

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const SequenceJobs = (job) => {
-    var sequence = "";
+const moveDependencyBeforeJob = (jobs, jobIndex, dependency) => {
+    // remove dependency from job list
+    jobs.splice(jobs.indexOf(dependency), 1)[0];
+
+    // re-insert dependency before job
+    jobs.splice(jobIndex, 0, dependency);
+    return jobs;
+};
+
+
+const SequenceJobs = (jobsList) => {
     // handles both single string and array of strings
-    var jobs = [].concat(job);
-    jobs.forEach(job => {
-        sequence += job.charAt(0);
+    var jobs = [].concat(jobsList);
+    let jobChars = jobs.map((job) => {
+        return job.charAt(0);
     });
 
-    return sequence;
+    jobs.forEach((job, jobIndex) => {
+        var jobParts = job.split("=>");
+        if (jobParts.length == 2 && jobParts[1] != "") {
+            let dependency = jobParts[1].trim();
+            jobChars = moveDependencyBeforeJob(jobChars, jobIndex, dependency)
+        }
+    });
+
+    return jobChars.join("");
 }
 
 export default SequenceJobs;
--- tests/sequencer.test.js
+++ tests/sequencer.test.js
@@ -1,4 +1,5 @@
 import sequenceJobs from "../main";
+import { expect } from "@jest/globals";
 
 describe("Job Sequencer", () => {
     it("returns an empty string when provided no jobs", () => {
@@ -18,6 +19,34 @@ describe("Job Sequencer", () => {
 
     it("returns a correctly sequenced list of jobs when provided a sequence of a jobs, one with a dependency", () => {
         var result = sequenceJobs(["a =>", "b => c", "c =>"]);
+
+        const bIndex = result.indexOf("b");
+        const cIndex = result.indexOf("c");
+        expect(cIndex).toBeLessThan(bIndex);
         expect(result).toBe("acb");
+    })
+
+    it("returns a correctly sequenced list of jobs when provided a sequence of a jobs with multiple dependencies", () => {
+        var result = sequenceJobs([
+            "a =>",
+            "b => c",
+            "c => f",
+            "d => a",
+            "e => b",
+            "f =>"]);
+
+        const aIndex = result.indexOf("a");
+        const bIndex = result.indexOf("b");
+        const cIndex = result.indexOf("c");
+        const dIndex = result.indexOf("d");
+        const eIndex = result.indexOf("e");
+        const fIndex = result.indexOf("f");
+
+        expect(fIndex).toBeLessThan(cIndex);
+        expect(cIndex).toBeLessThan(bIndex);
+        expect(bIndex).toBeLessThan(eIndex);
+        expect(aIndex).toBeLessThan(dIndex);
+        expect(result.length).toBe(6);
+        expect(result).toBe("fcabde");
     })
 })
